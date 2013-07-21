ljmljz/xpra

commit 13b234d962c866f63841dd005cd795489c828a81

workaround more Ubuntu needless breakage
git-svn-id: https://xpra.org/svn/Xpra/trunk@3897 3bb7dfac-3a0b-4e04-842a-767bc560f471

src/setup.py
@@ -410,6 +410,19 @@ def get_xorg_conf_and_script():
     else:
         print("Xdummy support unspecified, will try to detect")
 
+    cmd = ["lsb_release", "-cs"]
+    try:
+        proc = subprocess.Popen(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        out, _ = proc.communicate()
+        release = out.replace("\n", "")
+        print("Found OS release: %s" % release)
+        if release=="raring":
+            #yet another instance of Ubuntu breaking something
+            print("Warning: Ubuntu 'raring' breaks Xorg/Xdummy usage - using Xvfb fallback")
+            return  Xvfb()
+    except Exception, e:
+        print("failed to detect OS release using %s: e" % (" ".join(cmd), e))
+
     #do live detection
     cmd = ["Xorg", "-version"]
     print("detecting Xorg version using: %s" % str(cmd))

commit 1f3fc33b8cb42b7988557ddbc13dbaa5992da11b

fix error path string breakage caused by r3897

src/setup.py
@@ -424,7 +424,7 @@ def get_xorg_conf_and_script():
             print("Warning: Ubuntu 'raring' breaks Xorg/Xdummy usage - using Xvfb fallback")
             return  Xvfb()
     except Exception, e:
-        print("failed to detect OS release using %s: e" % (" ".join(cmd), e))
+        print("failed to detect OS release using %s: %s" % (" ".join(cmd), e))
 
     #do live detection
     cmd = ["Xorg", "-version"]