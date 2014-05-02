ljmljz/xpra

commit 0dec16c580531f0df95d1cd9c25942d5b334865e

gstreamer-1.0 / gi.repository compat: * remove old signal object superclass and use gobject (qt will need something else?) * setup GST_PLUGIN_PATH for win32 (and re-use get_app_dir rather than copying it) * provide compatibility aliases for new constant locations * define alternatives for codec options (as each version may have different elements available for the same encoding)
git-svn-id: https://www.xpra.org/svn/Xpra/trunk@6308 3bb7dfac-3a0b-4e04-842a-767bc560f471

src/xpra/sound/sink.py
@@ -6,7 +6,7 @@
 
 import sys, os, time
 
-from xpra.sound.sound_pipeline import SoundPipeline
+from xpra.sound.sound_pipeline import SoundPipeline, gobject, one_arg_signal
 from xpra.sound.pulseaudio_util import has_pa
 from xpra.sound.gstreamer_util import plugin_str, get_decoder_parser, MP3, CODECS, gst
 from xpra.os_util import thread
@@ -52,17 +52,17 @@ def sink_has_device_attribute(sink):
 
 class SoundSink(SoundPipeline):
 
-    __generic_signals__ = [
-        "underrun",
-        "overrun",
-        "eos"
-        ]
+    __gsignals__ = SoundPipeline.__generic_signals__.copy()
+    __gsignals__.update({
+        "underrun"  : one_arg_signal,
+        "overrun"   : one_arg_signal,
+        "eos"       : one_arg_signal,
+        })
 
     def __init__(self, sink_type=DEFAULT_SINK, options={}, codec=MP3, decoder_options={}):
         assert sink_type in SINKS, "invalid sink: %s" % sink_type
         decoder, parser = get_decoder_parser(codec)
         SoundPipeline.__init__(self, codec)
-        self.add_signals(self.__generic_signals__)
         self.sink_type = sink_type
         decoder_str = plugin_str(decoder, decoder_options)
         pipeline_els = []
@@ -78,15 +78,15 @@ class SoundSink(SoundPipeline):
         pipeline_els.append(decoder_str)
         pipeline_els.append("audioconvert")
         pipeline_els.append("audioresample")
-        queue_el =  "queue" + \
-                    " name=queue"+ \
-                    " max-size-buffers=0"+ \
-                    " max-size-bytes=0"+ \
-                    " max-size-time=%s" % QUEUE_TIME+ \
-                    " leaky=%s" % QUEUE_LEAK
+        queue_el =  ["queue",
+                    "name=queue"+ \
+                    "max-size-buffers=0"+ \
+                    "max-size-bytes=0"+ \
+                    "max-size-time=%s" % QUEUE_TIME+ \
+                    "leaky=%s" % QUEUE_LEAK]
         if QUEUE_SILENT:
-            queue_el.append(" silent=%s" % QUEUE_SILENT)
-        pipeline_els.append(queue_el)
+            queue_el.append("silent=%s" % QUEUE_SILENT)
+        pipeline_els.append(" ".join(queue_el))
         pipeline_els.append(sink_type)
         self.setup_pipeline_and_bus(pipeline_els)
         self.src = self.pipeline.get_by_name("src")
@@ -150,7 +150,7 @@ class SoundSink(SoundPipeline):
         #debug("sound sink: adding %s bytes to %s, metadata: %s, level=%s", len(data), self.src, metadata, int(self.queue.get_property("current-level-time")/MS_TO_NS))
         if not self.src:
             return
-        buf = gst.Buffer(data)
+        buf = gst.new_buffer(data)
         d = 10*MS_TO_NS
         if metadata and False:
             ts = metadata.get("timestamp")
@@ -180,6 +180,7 @@ class SoundSink(SoundPipeline):
         log("sound sink: pushed %s bytes, new buffer level: %sms", len(buf.data), ltime)
         return True
 
+gobject.type_register(SoundSink)
 
 
 def main():
@@ -187,7 +188,6 @@ def main():
     init("Sound-Record")
     try:
         import os.path
-        import gobject
         if len(sys.argv) not in (2, 3):
             print("usage: %s filename [codec]" % sys.argv[0])
             return 1

src/xpra/signal_object.py
@@ -1,60 +1,0 @@
-#!/usr/bin/env python
-# This file is part of Xpra.
-# Copyright (C) 2010-2014 Antoine Martin <antoine@devloop.org.uk>
-# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
-# later version. See the file COPYING for details.
-
-import itertools
-from xpra.log import Logger
-log = Logger("util")
-
-
-class SignalObject(object):
-    """
-        A very simple class for emulating gobject's emit()
-        So we can use this with Qt too.
-    """
-
-    def __init__(self, signals=[]):
-        self._signals = []
-        self._signal_listeners = {}
-        self._signal_id = itertools.count(1)
-        self.add_signals(signals)
-
-    def add_signals(self, signals):
-        self._signals += signals
-
-    def connect(self, signal, fn, *args):
-        assert signal in self._signals, "unknown signal: %s" % signal
-        listeners = self._signal_listeners.setdefault(signal, [])
-        sid = self._signal_id.next()
-        listeners.append((sid, fn, args))
-        return sid
-
-    def remove_listener(self, signal, sid):
-        assert signal in self._signals, "unknown signal: %s" % signal
-        listeners = self._signal_listeners.get(signal)
-        if not listeners:
-            log.warn("cannot remove signal listener %s: no listeners found for signal %s", sid, signal)
-            return
-        new_listeners = [x for x in listeners if x[0]!=sid]
-        if len(new_listeners)==len(listeners):
-            log.warn("cannot remove signal listener %s: not present in list for signal %s", sid, signal)
-            return
-        self._signal_listeners[signal] = new_listeners
-
-    def emit(self, signal, *args):
-        assert signal in self._signals, "unknown signal: %s" % signal
-        listeners = self._signal_listeners.get(signal)
-        if not listeners:
-            log("emit(%s, %s) no listeners", signal, args)
-            return
-        for _, fn, fnargs in listeners:
-            try:
-                allargs = [self]+list(fnargs)+list(args)
-                fn(*allargs)
-            except Exception, e:
-                log.error("error on listener %s for signal %s: %s", fn, signal, e, exc_info=True)
-
-    def cleanup(self):
-        self._signal_listeners = {}

src/xpra/sound/gstreamer_util.py
@@ -24,16 +24,21 @@ SPEEX = "speex"
 WAVPACK = "wavpack"
 
 #format: encoder, formatter, decoder, parser
-CODECS = {
+#we keep multiple options here for the same encoding
+#and will populate the ones that are actually available into the "CODECS" dict
+CODEC_OPTIONS = [
             #VORBIS : ("vorbisenc", "oggmux", "vorbisdec", "oggdemux"),
             #AAC     : ("faac",          "oggmux",   "faad",         "aacparse"),
-            FLAC    : ("flacenc",       "oggmux",   "flacdec",      "oggdemux"),
-            MP3     : ("lamemp3enc",    None,       "mad",          "mp3parse"),
-            WAV     : ("wavenc",        None,       None,           "wavparse"),
-            OPUS    : ("opusenc",       "oggmux",   "opusdec",      "oggdemux"),
-            SPEEX   : ("speexenc",      "oggmux",   "speexdec",     "oggdemux"),
-            WAVPACK : ("wavpackenc",    None,       "wavpackdec",   "wavpackparse"),
-            }
+            (FLAC        , "flacenc",       "oggmux",   "flacdec",      "oggdemux"),
+            (MP3         , "lamemp3enc",    None,       "mad",          "mp3parse"),
+            (MP3         , "lamemp3enc",    None,       "mad",          "mpegaudioparse"),
+            (WAV         , "wavenc",        None,       None,           "wavparse"),
+            (OPUS        , "opusenc",       "oggmux",   "opusdec",      "oggdemux"),
+            (SPEEX       , "speexenc",      "oggmux",   "speexdec",     "oggdemux"),
+            (WAVPACK     , "wavpackenc",    None,       "wavpackdec",   "wavpackparse"),
+            ]
+CODECS = {}
+
 CODEC_ORDER = [MP3, WAVPACK, WAV, FLAC, SPEEX]
 
 
@@ -59,19 +64,44 @@ def unredirect_stderr(oldfd):
         os.dup2(oldfd, 2)
 
 
+gst = None
+has_gst = False
+
 all_plugin_names = []
 pygst_version = ""
 gst_version = ""
 #ugly win32 hack to make it find the gstreamer plugins:
-if sys.platform.startswith("win"):
-    if hasattr(sys, "frozen"):
-        if sys.frozen in ("windows_exe", "console_exe"):
-            if sys.version > '3':
-                unicode = str           #@ReservedAssignment
-            main_dir = os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding()))
-            os.environ["GST_PLUGIN_PATH"] = os.path.join(main_dir, "gstreamer-0.10")
+if sys.platform.startswith("win") and hasattr(sys, "frozen") and sys.frozen in ("windows_exe", "console_exe", True):
+    from xpra.platform.paths import get_app_dir
+    if sys.version_info[0]<3:
+        #gstreamer-0.10
+        v = (0, 10)
+    else:
+        #gstreamer-1.0
+        v = (1, 0)
+    os.environ["GST_PLUGIN_PATH"] = os.path.join(get_app_dir(), "gstreamer-%s" % (".".join([str(x) for x in v])))
 
-try:
+
+def import_gst1():
+    import gi
+    from gi.repository import Gst           #@UnresolvedImport
+    #gi.require_version('Gst', '0.10')
+    gi.require_version('Gst', '1.0')
+    Gst.init(None)
+    #make it look like pygst (gstreamer-0.10):
+    Gst.registry_get_default = Gst.Registry.get 
+    Gst.get_pygst_version = lambda: gi.version_info
+    Gst.get_gst_version = lambda: Gst.version()
+    Gst.new_buffer = Gst.Buffer.new_allocate
+    #note: we only copy the constants we actually need..
+    for x in ('NULL', 'PAUSED', 'PLAYING', 'READY', 'VOID_PENDING'):
+        setattr(Gst, "STATE_%s" % x, getattr(Gst.State, x))
+    for x in ('EOS', 'ERROR', 'TAG', 'STREAM_STATUS', 'STATE_CHANGED',
+              'LATENCY', 'WARNING', 'ASYNC_DONE', 'NEW_CLOCK', 'DURATION'):
+        setattr(Gst, "MESSAGE_%s" % x, getattr(Gst.MessageType, x))
+    return Gst
+
+def import_gst0_10():
     import pygst
     pygst.require("0.10")
     try:
@@ -80,20 +110,57 @@ try:
         #so we temporarily replace them:
         saved_args = sys.argv
         sys.argv = sys.argv[:1]
-
         #now do the import with stderr redirection
         #to avoid gobject warnings:
         oldfd = redirect_stderr()
         import gst
-        has_gst = True
         gst_version = gst.gst_version
         pygst_version = gst.pygst_version
+        gst.new_buffer = gst.Buffer
+        return gst
     finally:
         unredirect_stderr(oldfd)
         sys.argv = saved_args
-except:
-    has_gst = False
 
+try:
+    from xpra.gtk_common.gobject_compat import is_gtk3
+    if is_gtk3():
+        gst = import_gst1()
+    else:
+        gst = import_gst0_10()
+    has_gst = True
+except:
+    log("failed to import GStreamer", exc_info=True)
+
+
+def get_all_plugin_names():
+    global all_plugin_names, has_gst
+    if len(all_plugin_names)==0 and has_gst:
+        registry = gst.registry_get_default()
+        all_plugin_names = [el.get_name() for el in registry.get_feature_list(gst.ElementFactory)]
+        all_plugin_names.sort()
+        log("found the following plugins: %s", all_plugin_names)
+    return all_plugin_names
+
+def has_plugins(*names):
+    allp = get_all_plugin_names()
+    missing = [name for name in names if (name is not None and name not in allp)]
+    if len(missing)>0:
+        log("missing %s from %s (all=%s)", missing, names, allp)
+    return len(missing)==0
+
+
+if has_gst:
+    #populate CODECS:
+    for elements in CODEC_OPTIONS:
+        #verify we have all the elements needed:
+        if has_plugins(*elements[1:]):
+            #ie: FLAC, "flacenc", "oggmux", "flacdec", "oggdemux" = elements
+            encoding, encoder, muxer, decoder, demuxer = elements
+            CODECS[encoding] = (encoder, muxer, decoder, demuxer)
+    log("initalized CODECS:")
+    for k in [x for x in CODEC_ORDER if x in CODECS]:
+        log("* %s : %s", k, CODECS[k])
 
 def get_sound_codecs(is_speaker, is_server):
     global has_gst
@@ -104,7 +171,8 @@ def get_sound_codecs(is_speaker, is_server):
             return can_encode()
         else:
             return can_decode()
-    except Exception, e:
+    except:
+        e = sys.exc_info()[1]
         log.warn("failed to get list of codecs: %s" % e)
         return []
 
@@ -138,43 +206,29 @@ def show_sound_codec_help(is_server, speaker_codecs, microphone_codecs):
     return hm or hs
 
 
-def get_all_plugin_names():
-    global all_plugin_names, has_gst
-    if len(all_plugin_names)==0 and has_gst:
-        registry = gst.registry_get_default()
-        all_plugin_names = [el.get_name() for el in registry.get_feature_list(gst.ElementFactory)]
-        all_plugin_names.sort()
-        log("found the following plugins: %s", all_plugin_names)
-    return all_plugin_names
-
-def has_plugins(*names):
-    allp = get_all_plugin_names()
-    missing = [name for name in names if (name is not None and name not in allp)]
-    if len(missing)>0:
-        log("missing %s from %s (all=%s)", missing, names, allp)
-    return len(missing)==0
-
 def get_encoder_formatter(name):
-    assert name in CODECS, "invalid codec: %s" % name
+    assert name in CODECS, "invalid codec: %s (should be one of: %s)" % (name, CODECS.keys())
     encoder, formatter, _, _ = CODECS.get(name)
     assert encoder is None or has_plugins(encoder), "encoder %s not found" % encoder
     assert formatter is None or has_plugins(formatter), "formatter %s not found" % formatter
     return encoder, formatter
 
 def get_decoder_parser(name):
-    assert name in CODECS, "invalid codec: %s" % name
+    assert name in CODECS, "invalid codec: %s (should be one of: %s)" % (name, CODECS.keys())
     _, _, decoder, parser = CODECS.get(name)
     assert decoder is None or has_plugins(decoder), "decoder %s not found" % decoder
     assert parser is None or has_plugins(parser), "parser %s not found" % parser
     return decoder, parser
 
 def has_encoder(name):
-    assert name in CODECS, "invalid codec: %s" % name
+    if name not in CODECS:
+        return False
     encoder, fmt, _, _ = CODECS.get(name)
     return has_plugins(encoder, fmt)
 
 def has_decoder(name):
-    assert name in CODECS, "invalid codec: %s" % name
+    if name not in CODECS:
+        return False
     _, _, decoder, parser = CODECS.get(name)
     return has_plugins(decoder, parser)
 
@@ -281,7 +335,8 @@ def start_sending_sound(codec, volume, remote_decoders, local_decoders, remote_p
             sound_source = SoundSource("pulsesrc", {"device" : monitor_device}, codec, volume, {})
             log.info("starting sound capture using pulseaudio device: %s", monitor_device_name)
         return sound_source
-    except Exception, e:
+    except:
+        e = sys.exc_info()[1]
         log.error("error setting up sound: %s", e, exc_info=True)
         return    None
 

src/xpra/sound/sound_pipeline.py
@@ -5,23 +5,25 @@
 
 import time
 
-from xpra.signal_object import SignalObject
 from xpra.sound.gstreamer_util import gst
 from xpra.log import Logger
 log = Logger("sound")
 
+from xpra.gtk_common.gobject_compat import import_gobject
+from xpra.gtk_common.gobject_util import one_arg_signal
+gobject = import_gobject()
 
-class SoundPipeline(SignalObject):
 
-    __generic_signals__ = [
-        "state-changed",
-        "bitrate-changed",
-        "error"
-        ]
+class SoundPipeline(gobject.GObject):
+
+    __generic_signals__ = {
+        "state-changed"     : one_arg_signal,
+        "bitrate-changed"   : one_arg_signal,
+        "error"             : one_arg_signal,
+        }
 
     def __init__(self, codec):
-        SignalObject.__init__(self)
-        self.add_signals(SoundPipeline.__generic_signals__)
+        gobject.GObject.__init__(self)
         self.codec = codec
         self.codec_description = codec
         self.codec_mode = ""
@@ -89,7 +91,6 @@ class SoundPipeline(SignalObject):
 
     def cleanup(self):
         log("SoundPipeline.cleanup()")
-        SignalObject.cleanup(self)
         self.stop()
         if self.bus:
             self.bus.remove_signal_watch()

src/xpra/sound/src.py
@@ -5,9 +5,9 @@
 # later version. See the file COPYING for details.
 
 import sys, os
-import gobject
 
-from xpra.sound.sound_pipeline import SoundPipeline
+from xpra.sound.sound_pipeline import SoundPipeline, gobject
+from xpra.gtk_common.gobject_util import n_arg_signal
 from xpra.sound.pulseaudio_util import has_pa
 from xpra.sound.gstreamer_util import plugin_str, get_encoder_formatter, MP3, CODECS
 from xpra.log import Logger
@@ -40,15 +40,15 @@ AUDIORESAMPLE = False
 
 class SoundSource(SoundPipeline):
 
-    __generic_signals__ = [
-        "new-buffer"
-        ]
+    __gsignals__ = SoundPipeline.__generic_signals__.copy()
+    __gsignals__.update({
+        "new-buffer"    : n_arg_signal(2),
+        })
 
     def __init__(self, src_type=DEFAULT_SRC, src_options={}, codec=MP3, volume=1.0, encoder_options={}):
         assert src_type in SOURCES
         encoder, fmt = get_encoder_formatter(codec)
         SoundPipeline.__init__(self, codec)
-        self.add_signals(self.__generic_signals__)
         self.src_type = src_type
         source_str = plugin_str(src_type, src_options)
         encoder_str = plugin_str(encoder, encoder_options)
@@ -111,6 +111,8 @@ class SoundSource(SoundPipeline):
         self.buffer_count += 1
         self.byte_count += len(buf.data)
         self.emit("new-buffer", buf.data, metadata)
+
+gobject.type_register(SoundSource)
 
 
 def main():

commit 26d3871750e41f9cddf7d02ce07c1b2f552e65ea

fix sound breakage introduced in r6308: queue_el is now a list of attributes

src/xpra/sound/sink.py
@@ -79,10 +79,10 @@ class SoundSink(SoundPipeline):
         pipeline_els.append("audioconvert")
         pipeline_els.append("audioresample")
         queue_el =  ["queue",
-                    "name=queue"+ \
-                    "max-size-buffers=0"+ \
-                    "max-size-bytes=0"+ \
-                    "max-size-time=%s" % QUEUE_TIME+ \
+                    "name=queue",
+                    "max-size-buffers=0",
+                    "max-size-bytes=0",
+                    "max-size-time=%s" % QUEUE_TIME,
                     "leaky=%s" % QUEUE_LEAK]
         if QUEUE_SILENT:
             queue_el.append("silent=%s" % QUEUE_SILENT)